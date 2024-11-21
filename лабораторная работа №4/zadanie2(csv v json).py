6b8c3f00e6563c2f39e504f8336ab5447438782e
Ivan21001 <vaniabockarev@gmail.com>
2024-11-21 18:25:43 +0300

    Update zadanie2(csv v json).py

--- лабораторная работа №4/zadanie2(csv v json).py
+++ лабораторная работа №4/zadanie2(csv v json).py
@@ -8,10 +8,12 @@ OUTPUT_FILENAME = "output.json"
 
 def task(file: csv) -> None:
     with open(file, "r", encoding="utf-8") as f:
-        content = csv.DictReader(f)
+        content = csv.DictReader(f, delimiter=',', quotechar='\n')
+        data: list = []
+        for row_ in content:
+            data.append(row_)
         with open(OUTPUT_FILENAME, 'w', encoding="utf-8") as new_file:
-            for row in content:
-                json.dump(row, new_file, indent=4, ensure_ascii=False)
+            json.dump(data, new_file, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
